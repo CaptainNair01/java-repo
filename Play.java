2d49387aa50f5f2165ee9a1e667881b829c7e294
Arnav <nairarn04@gmail.com>
2021-10-15 21:37:35 -0500

    commit

--- Play.java
+++ Play.java
@@ -22,11 +22,40 @@ public class Play {
             if(response == random){
                 counter++;
                 System.out.println("You guessed correctly and it took you " + counter + " tries!!!");
-                break;
+                game2();
             }
         }
+        scan.close();
     }
 
+    public static void game2(){
+        System.out.println("Do you want to play another game? ");
+        Scanner scan2 = new Scanner(System.in);
+        String res = scan2.nextLine();
+        String lower = res.toLowerCase();
+        if(lower.equals("yes")){
+            System.out.println("Pick a number: ");
+            Scanner scan3 = new Scanner(System.in);
+            int res2 = scan3.nextInt();
+            int rand = (int) Math.floor(Math.random() * 101);
+            if(res2>rand){
+                System.out.println("You Picked: " + res2);
+                System.out.println("Computer Picked: " + rand);
+                System.out.println("YOU ARE A WINNER!!!!");
+            } else if(res2<rand){
+                System.out.println("You Picked: " + res2);
+                System.out.println("Computer Picked: " + rand);
+                System.out.println("YOU ARE A LOSER!!!!");
+            } else{
+                System.out.println("You Picked: " + res2);
+                System.out.println("Computer Picked: " + rand);
+                System.out.println("IT IS A TIE!!!!");
+            }
+        } else{
+            System.out.println("UNACCEPTABLE!!!!");
+        }
+        scan2.close();
+    }
     public static void main(String[] args) {
         game();
     }
